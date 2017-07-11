50a26181be09bf65e0187c9501b95d48783d24a5
rohitporoli <rohitp4all@gmail.com>
2017-07-11 15:11:49 +0530

    mido: Add device maintainer info
    *showcase myself
    *gain popularity
    *show-off

--- citrus.mk
+++ citrus.mk
@@ -55,3 +55,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="xiaomi/tissot/tissot_sprout:7.1.2/N2G47H/7.8.23:user/release-keys" \
     PRIVATE_BUILD_DESC="tissot-user 7.1.2 N2G47H 7.8.23 release-keys"
+
+# Citrus-CAF Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+         DEVICE_MAINTAINERS="Satyabrat (thelazyindian)"

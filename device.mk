ff82180a3d9c81a511855f73a71ce162cd236744
thelazyindian <satyabrat.me@gmail.com>
2018-01-23 13:44:57 +0000

    tissot Add seccomp_policy

--- device.mk
+++ device.mk
@@ -380,7 +380,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
 
 # Sensors
 PRODUCT_COPY_FILES += \

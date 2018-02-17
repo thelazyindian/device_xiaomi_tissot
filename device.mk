e631cb04bc5e1f1e8e42a78cab9e0b5d35a82569
ghpranav <bedrepranav@gmail.com>
2018-02-17 15:16:39 +0530

    Tissot: Fix build errors

--- device.mk
+++ device.mk
@@ -114,8 +114,8 @@ PRODUCT_PACKAGES += \
     Snap
 
 # Consumerir
-PRODUCT_PACKAGES += \
-    consumerir.msm8953
+#PRODUCT_PACKAGES += \
+#    consumerir.msm8953
 
 # Display
 PRODUCT_PACKAGES += \

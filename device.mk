41e8d53dd33c65deb3d4cb72e1a8079fd7f37564
dianlujitao <dianlujitao@lineageos.org>
2018-02-20 22:47:08 +0800

    tissot: Build LiveDisplay HAL
    Change-Id: I4bfac85c64599ced45983a86d98cfd810a97db99

--- device.mk
+++ device.mk
@@ -304,6 +304,10 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-qdcm
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/vendor/etc/media_codecs.xml \

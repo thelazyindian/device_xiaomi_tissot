rohitporoli/device_xiaomi_mido

mido: mka lemonade
Signed-off-by: rohitporoli <rohitp4all@gmail.com>

citrus.mk
@@ -16,10 +16,10 @@
 
 $(call inherit-product, device/xiaomi/mido/full_mido.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common citrus peel.
+$(call inherit-product, vendor/citrus/common.mk)
 
-PRODUCT_NAME := lineage_mido
+PRODUCT_NAME := citrus_mido
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi

thelazyindian/device_xiaomi_tissot

citrus.mk
@@ -16,8 +16,8 @@
 
 $(call inherit-product, device/xiaomi/tissot/full_tissot.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common citrus peel.
+$(call inherit-product, vendor/citrus/common.mk)
 
 # A/B updater
 AB_OTA_UPDATER := true
@@ -47,7 +47,7 @@ PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     librecovery_updater_msm8953 \
     libsparse_static
 
-PRODUCT_NAME := lineage_tissot
+PRODUCT_NAME := citrus_tissot
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
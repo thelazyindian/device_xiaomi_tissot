db755981100a7c6c24b4ca2e5329685adfc4b25d
thelazyindian <satyabrat.me@gmail.com>
2018-02-21 09:44:47 +0000

    tissot: Nuke modules
    wlan modules are now built into kernel itself

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -224,8 +224,6 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
-WIFI_DRIVER_MODULE_NAME := "wlan"
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/tissot/BoardConfigVendor.mk

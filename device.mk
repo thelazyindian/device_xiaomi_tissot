94e604b930ab3be6c3a7ce1cbd21802943cdf0bc
Artem Borisov <dedsa2002@gmail.com>
2018-02-26 19:13:23 +0300

    tissot: Add dummy libkeymaster1 for goodix fingerprint

--- device.mk
+++ device.mk
@@ -213,7 +213,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service \
+    libkeymaster1
 
 # For config.fs
 PRODUCT_PACKAGES += \

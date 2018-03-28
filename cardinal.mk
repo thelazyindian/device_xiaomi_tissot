e247282080ff6735e1ad04dec2ad2f08fc58f8fc
Adarsh-MR <adarshmr1998@gmail.com>
2018-03-28 09:28:03 +0000

    tissot: Initial modification for Cardinal-AOSP
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- cardinal.mk
+++ cardinal.mk
@@ -17,9 +17,9 @@
 $(call inherit-product, device/xiaomi/tissot/full_tissot.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cardinal/common.mk)
 
-PRODUCT_NAME := lineage_tissot
+PRODUCT_NAME := cardinal_tissot
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
